cf548d40ea3d7584641a8f22bd6203a0e87f53b2
mcarlile <mckenzie.carlile@gmail.com>
2014-01-29 19:32:32 -0800

    Alpha 1.5
    First Prototype!

---
+++ Assets/Manager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour
+{
+		public GameObject aBox;
+		public GameObject bBox;
+		public GameObject cBox;
+		public GameObject dBox;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+
+
+		}
+}
---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gate : MonoBehaviour
+{
+
+		public Color openColor;
+		public Color closedColor;
+		public string keyCode;
+		public GameObject aBox;
+		public GameObject bBox;
+		public GameObject cBox;
+		public GameObject dBox;
+	
+	
+		// Use this for initialization
+		void Start ()
+		{
+				aBox.gameObject.renderer.material.SetColor ("_Color", closedColor);
+				bBox.gameObject.renderer.material.SetColor ("_Color", closedColor);
+				cBox.gameObject.renderer.material.SetColor ("_Color", closedColor);
+				dBox.gameObject.renderer.material.SetColor ("_Color", closedColor);
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Input.GetKey (KeyCode.A)) {
+						aBox.renderer.material.SetColor ("_Color", openColor);
+						aBox.gameObject.collider.isTrigger = true;
+				} else {
+						aBox.renderer.material.SetColor ("_Color", closedColor);
+						aBox.gameObject.collider.isTrigger = false;
+				}
+		
+				if (Input.GetKey (KeyCode.B)) {
+						bBox.renderer.material.SetColor ("_Color", openColor);
+						bBox.gameObject.collider.isTrigger = true;
+				} else {
+						bBox.renderer.material.SetColor ("_Color", closedColor);
+						bBox.gameObject.collider.isTrigger = false;
+				}
+
+				if (Input.GetKey (KeyCode.C)) {
+						cBox.renderer.material.SetColor ("_Color", openColor);
+						cBox.gameObject.collider.isTrigger = true;
+				} else {
+						cBox.renderer.material.SetColor ("_Color", closedColor);
+						cBox.gameObject.collider.isTrigger = false;
+				}
+
+				if (Input.GetKey (KeyCode.D)) {
+						dBox.renderer.material.SetColor ("_Color", openColor);
+						dBox.gameObject.collider.isTrigger = true;
+				} else {
+						dBox.renderer.material.SetColor ("_Color", closedColor);
+						dBox.gameObject.collider.isTrigger = false;
+				}
+		}
+	
+}
